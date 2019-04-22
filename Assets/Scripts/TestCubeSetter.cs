aaaf225e3037e5c114f70d6d2bd51aeef9d71763
s1500729 <s1500729@sendai-nct.jp>
2019-04-22 21:40:26 +0900

    タイルの頂点取得を実装

---
+++ Assets/Scripts/TestCubeSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestCubeSetter : MonoBehaviour
+{
+	[SerializeField]
+	Tilemap map;
+
+	private void Awake()
+	{
+		var vecs = map.GetVertices(new Vector3Int(0, 0, 0));
+
+		foreach(var vec in vecs)
+		{
+			var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+			cube.transform.position = vec;
+		}
+	}
+}
---
+++ Assets/Scripts/TileExtend.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class TileExtend 
+{
+    public static IEnumerable<Vector3> GetVertices(this Tilemap map, Vector3Int position)
+	{
+		var center = map.GetCellCenterWorld(position);
+
+		var size = map.cellSize / 2;
+
+		// 上半分の頂点を追加
+		var deltas = new[]
+		{
+			new Vector3(size.x, size.y * Mathf.Sin(Mathf.PI / 6f)),
+			new Vector3(0, size.y),
+			new Vector3(-size.x, size.y * Mathf.Sin(Mathf.PI / 6f))
+		}.ToList();
+		
+		// 下半分を追加
+		for(int i = 0;i < 3;i++)
+		{
+			deltas.Add(new Vector3(deltas[i].x, -deltas[i].y));
+		}
+		
+		// 原点を移動
+		return deltas.Select(delta => center + delta);
+	}
+}
