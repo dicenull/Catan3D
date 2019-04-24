61f8998ef0c4c730cfbaf8a2c7cd27947f76bfc4
s1500729 <s1500729@sendai-nct.jp>
2019-04-24 23:56:25 +0900

    単純なルームへのネットワーク接続を実装

---
+++ Assets/Scripts/PunNetwork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PunNetwork : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		PhotonNetwork.ConnectUsingSettings();
+    }
+	
+	public override void OnConnectedToMaster()
+	{
+		PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("join");
+	}
+}
